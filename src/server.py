33c0671b609fd93194889d87b46d005ef0ca98b3
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2021-08-08 11:17:09 -0300

    Adding status code for all the routes

--- src/server.py
+++ src/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status
 from sqlalchemy.orm import Session
 from src.schemas import schemas
 from src.infra.sqlalchemy.repository.product import RepositoryProduct
@@ -10,22 +10,22 @@ database.create_db()
 app = FastAPI()
 
 # Depends injeção de dependência
-@app.post('/product')
+@app.post('/products', status_code=status.HTTP_201_CREATED)
 def create_products(product: schemas.Product, database: Session = Depends(database.get_db)):
   created_product = RepositoryProduct(database).create(product)
   return created_product
 
-@app.get('/products')
+@app.get('/products', status_code=status.HTTP_200_OK)
 def list_products(database: Session = Depends(database.get_db)):
   products = RepositoryProduct(database).index()
   return products
 
-@app.post('/user')
+@app.post('/user', status_code=status.HTTP_200_OK)
 def create_users(user: schemas.User, database: Session = Depends(database.get_db)):
   create_user = RepositoryUser(database).create(user)
   return create_user
 
-@app.get('/users')
+@app.get('/users', status_code=status.HTTP_201_CREATED)
 def list_users(database: Session = Depends(database.get_db)):
   users = RepositoryUser(database).index()
   return users
